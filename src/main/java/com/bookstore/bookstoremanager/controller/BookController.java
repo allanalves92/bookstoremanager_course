a5fe94f0ec1c1eded124eb50536e21a7cc5b406c
Allan Alves <allanalvesmail@gmail.com>
2021-03-14 13:37:00 -0300

    Created .travis.yml file

--- src/main/java/com/bookstore/bookstoremanager/controller/BookController.java
+++ src/main/java/com/bookstore/bookstoremanager/controller/BookController.java
@@ -1,15 +1,18 @@
 package com.bookstore.bookstoremanager.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
 
+    @ApiOperation(value = "Return an example hello world")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success method return")
+    })
     @GetMapping
-    public String hello(){
+    public String hello() {
         return "Hello Bookstore Manager";
     }
 
